2332f11f80c6e56062dc83882221eaf9776af21a
Angel421-oss <miguel000ochoa@gmail.com>
2024-08-17 19:48:43 -0500

    continuacion de generacion de contraseña

--- generador de contraseñas.py
+++ generador de contraseñas.py
@@ -4,8 +4,14 @@ import random
 import string
 
 # Funcion 
-def generate_password(length=12):
+def generate_password(longitud=12):
     characters = string.ascii_letters + string.digits + string.punctuation
+    contrasena = ''.join(random.choice(characters)for _ in range(longitud))
+    return contrasena
+
+longitud_deseada=int(input("Indique los en numero de caracteres de la contrasena: "))
+nueva_contrasena=generate_password(longitud_deseada)
+print(f'Esta es tu contrasena --> {nueva_contrasena}')
 
 
     
